e57ce52cd1a282ae8d60abfa5b5fd1a418ac6c99
Jonathan Ford <jonford3@hotmail.com>
2015-09-14 17:49:06 -0700

    Initial file hash of httpd_file_content and return some of the values. Still need to do array returns

--- lib/config/httpd_conf.rb
+++ lib/config/httpd_conf.rb
@@ -4,30 +4,50 @@ require_relative 'configuration'
 module WebServer
   class HttpdConf < Configuration
     def initialize(httpd_file_content)
+      @httpd_file_content = httpd_file_content
+      @hash = Hash.new
+      @httpd_file_content.split("\n").each do |pair|
+        key,value = pair.split(" ")
+        @hash[key] = value
+      end
+      #Line below prints key/value pairs to console for testing
+      #@hash.each {|key, value| puts "KEY is [#{key}] VALUE is [#{value}]\n\n" }
     end
 
     # Returns the value of the ServerRoot
     def server_root 
+      @server_root = @hash.fetch("ServerRoot").sub!("\"", "").chomp("\"")
+      return @server_root
     end
 
     # Returns the value of the DocumentRoot
     def document_root
+      @document_root = @hash.fetch("DocumentRoot").sub!("\"", "").chomp("\"")
+      return @document_root
     end
 
     # Returns the directory index file
     def directory_index
+      @directory_index = @hash.fetch("DirectoryIndex")
+      return @directory_index
     end
 
     # Returns the *integer* value of Listen
     def port
+      @port = @hash.fetch("Listen")
+      return @port.to_i
     end
 
     # Returns the value of LogFile
     def log_file
+      @log_file = @hash.fetch("LogFile").sub!("\"", "").chomp("\"")
+      return @log_file      
     end
 
     # Returns the name of the AccessFile 
     def access_file_name
+      @access_file_name = @hash.fetch("AccessFileName")
+      return @access_file_name
     end
 
     # Returns an array of ScriptAlias directories
